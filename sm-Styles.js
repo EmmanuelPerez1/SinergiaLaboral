83127a636409fbea47fd1dd63e2c370065ab5b7e
EmmanuelPerez1 <arreolae475@gmail.com>
2019-05-07 19:53:57 -0500

    Intento de mejora para moviles

--- sm-Styles.js
+++ sm-Styles.js
@@ -5,6 +5,8 @@ const smSize = 576;
 function changeTextAlingClassToSm() {
 	if(heightWindow <= smSize || widthWindow <= smSize){
 		document.getElementById("footer").classList.add("text-center");
+		document.getElementById("espaces-to-remove").remove();
 	}
 }
+
 window.onresize = changeTextAlingClassToSm;
