c655dfb42d3b65b859b2f9f934c4040e9c967b14
Emmanuel Pérez <arreolae475@gmail.com>
2019-05-14 12:45:08 -0500

    Se añade Jumbotron sin estilos

--- scripts/sm-Styles.js
+++ scripts/sm-Styles.js
@@ -5,7 +5,12 @@ const smSize = 576;
 function changeTextAlingClassToSm() {
 	if(heightWindow <= smSize || widthWindow <= smSize){
 		document.getElementById("footer").classList.add("text-center");
-		document.getElementById("espaces-to-remove").remove();
+		var espaces = document.getElementsByClassName("espaces-to-remove");
+
+		for (var i = 0; i < espaces.length; i++) {
+			espaces[i].remove("espaces-to-remove")
+		}
+
 	}
 }
 
