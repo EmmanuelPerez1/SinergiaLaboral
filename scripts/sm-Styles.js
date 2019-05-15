c4f319b9f4ebfb377f72329ca3bdf12b7db88513
EmmanuelPerez1 <arreolae475@gmail.com>
2019-05-14 22:59:09 -0500

    Mejoras en vista para celular

--- scripts/sm-Styles.js
+++ scripts/sm-Styles.js
@@ -5,13 +5,21 @@ const smSize = 576;
 function changeTextAlingClassToSm() {
 	if(heightWindow <= smSize || widthWindow <= smSize){
 		document.getElementById("footer").classList.add("text-center");
-		var espaces = document.getElementsByClassName("espaces-to-remove");
 
-		for (var i = 0; i < espaces.length; i++) {
-			espaces[i].remove("espaces-to-remove")
-		}
-
+		removeEspacesForSm();
 	}
 }
+
+function removeEspacesForSm(){
+	var espaces = document.getElementsByClassName("espaces-to-remove");
+	for (var i = 0; i < espaces.length; i++) {
+		espaces[i].remove("espaces-to-remove")
+	}
+}
+
+// function resizeVideoMainPage(){
+// 	var video = document.getElementsByTagName("iframe")
+// 	video.
+// }
 
 window.onresize = changeTextAlingClassToSm;
