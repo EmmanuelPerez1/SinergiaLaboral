7b3bc80641acde7f07ad44d6c84b4c02f3efa5b9
EmmanuelPerez1 <arreolae475@gmail.com>
2019-05-15 23:26:47 -0500

    Página index optimizada para vistas mobiles

--- scripts/sm-Styles.js
+++ scripts/sm-Styles.js
@@ -6,7 +6,6 @@ function changeTextAlingClassToSm() {
 	if(heightWindow <= smSize || widthWindow <= smSize){
 		document.getElementById("footer").classList.add("text-center");
 		removeEspacesForSm();
-		// removeJumbotronForSm();
 		changeAligntoTextForSm();
 	}
 }
@@ -18,17 +17,13 @@ function removeEspacesForSm(){
 	}
 }
 
-function removeJumbotronForSm(){
-	var jumbotron = document.getElementsByClassName("j-color")
-	jumbotron[0].remove("j-color")
-}
-
 function changeAligntoTextForSm(){
-	var h1p = document.getElementsByClassName("sm-size")
+	var h1p = document.getElementsByClassName("sm-size");
 	for (var i = 0; i < h1p.length; i++) {
 		h1p[i].style.textAlign = "center";
 		h1p[i].style.fontSize = "1.8em";
 	}
 }
+
 
 window.onresize = changeTextAlingClassToSm;
